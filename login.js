d15dab411ae0c61f049c426c6244f5ebbdb1143c
tuanha1508 <tuanafk2006@gmail.com>
2025-01-18 13:09:32 -0500

    update login UI

--- login.js
+++ login.js
@@ -14,6 +14,7 @@ firebase.initializeApp(firebaseConfig);
 document.getElementById("login-form").addEventListener("submit", function (e) {
     e.preventDefault();
     const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
     if (username) {
         localStorage.setItem("sudokuUsername", username); 
